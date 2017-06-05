ec98bee01f9e0929cd84dab86ac7e26b9cb07a16
LuizBahia <noreply>
2017-06-05 12:11:32 +0000

    Add component vacilotto

---
+++ ui/vacilotto.reel/vacilotto.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/vacilotto.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Vacilotto
+ * @extends Component
+ */
+exports.Vacilotto = Component.specialize(/** @lends Vacilotto# */ {
+    constructor: {
+        value: function Vacilotto() {
+            this.super();
+        }
+    }
+});
